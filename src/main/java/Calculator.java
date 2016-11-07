1ce8fd21ab144c427cb70b7dcd86241af0ba6cb3
chester chua <chester chua>
2016-11-07 10:49:00 +0800

    changes to src

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-  public int evaluate(String expression) {
+  private int evaluate(String expression) {
     int sum = 0;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
