2028a64d43b66ce54c95bb139c6e0ca3df99cd3b
chester chua <chester chua>
2016-11-07 11:07:38 +0800

    change back to public for evaulate()

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-  private int evaluate(String expression) {
+  public int evaluate(String expression) {
     int sum = 0;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
